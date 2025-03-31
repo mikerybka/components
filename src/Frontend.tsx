ee2934f75bd9e03324dbaf747e2fd40eef00fcd9
Mike Rybka <merybka@gmail.com>
2025-03-31 14:50:28 -0400

    Create Frontend

---
+++ src/Frontend.tsx
@@ -0,0 +1,11 @@
+interface FrontendProps {
+    path: string;
+    userID: string;
+    sessionToken: string;
+}
+
+export const Frontend = ({path}: FrontendProps) => {
+    return <view>
+        
+    </view>
+}
